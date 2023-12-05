ab192e7893eb0dd8c641a337f7259160662cd277
M-Taha95echo <msoudi2013@gmail.com>
2023-12-05 14:09:38 +0300

    install rest_framwork

--- soft/models.py
+++ soft/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Drinks(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'{self.name}  {self.description} '
